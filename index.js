28635376f696eca12b1bdacd78edc8564dfdd74e
hunnerrose <114459778+hunnerrose@users.noreply.github.com>
2023-02-09 15:46:46 -0800

    Adding a 404 page route

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 //Require needed modules
 const express = require('express')
+require('dotenv').config()
+
+//Initialize app
 const app = express()
+
 
 app.get('/', (req,res) => {
     res.send('Hello world!')
 })
 
-app.listen(8083)
+//Make a Wildcard Route (for 404 errors): It's important that this route is at the bottom, otherwise it will override your other pages.
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
