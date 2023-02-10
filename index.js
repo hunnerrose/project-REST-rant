edc1591dbd2f1aac2bb20e95a064bf0ce124fa90
hunnerrose <114459778+hunnerrose@users.noreply.github.com>
2023-02-09 16:41:39 -0800

    Adding remaining stub routes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', (req,res) => {
     res.send('Hello world!')
 })
 
-//Make a Wildcard Route (for 404 errors): It's important that this route is at the bottom, otherwise it will override your other pages.
+//Make a Wildcard Route (for 404 errors): It's important that this route is at the bottom, otherwise it will override your other pages..
 app.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
