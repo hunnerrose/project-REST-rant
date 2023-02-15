d2a967da528959aec58d5c66fa9fa7460e5796f8
hunnerrose <114459778+hunnerrose@users.noreply.github.com>
2023-02-14 19:39:11 -0800

    Jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -1,27 +1,48 @@
 const router = require('express').Router()
+let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/thai-food.jpg'
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/coffee-creamer.jpg'
+}]
 
+//CREATE?
+router.post('/', (req, res) => {
+    console.log(req);
+    if (!req.body.pic) {
+        //Default image if one is not provided
+        req.body.pic = 'http://placekitten.com/400/400';
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown';
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA';
+    }
+    places.push(req.body);
+    res.redirect('/places');
+});
+
+//NEW
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
+//SHOW??
 router.get('/', (req, res) => {
     //for now we'll use mock data
         //how can I resize just these images?? v
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/thai-food.jpg'
-    }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/coffee-creamer.jpg'
-    }]
+
     //Pass the above place arary into the render method to allow us to use that array inside our view
     res.render('places/index', { places })
 })
+
 
 module.exports = router
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -8,6 +8,33 @@ function new_form() {
         <Def>
             <main>
                 <h1>Add a New Place</h1>
+                {/* Create a form tag w/ two crutial attributes: method 'HTTP verb' and action 'destination path' */}
+                <form method="POST" action="/places">
+                    {/* Create a div 'block element so each form field will live on a separate line' and in that div, create a label and an input. */}
+                        {/* Let's update the label and input with specific information. The most important part to remember is the name attribute of the input tag. This is important because it will be the variable name we end up using on the back-end later. Less critical, but still important, is to make a for attribute on the label that corresponds to an id attribute on the input. That is for accessibility/screen readers! */}
+                    <div className="form-group">
+                        <label htmlFor="name">Place Name</label>
+                        <input className="form-control" type="text" id="name" name="name" required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="pic">Place Picture</label>
+                        <input className="form-control" type="url" id="pic" name="pic" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="city">City</label>
+                        <input className="form-control" type="text" id="city" name="city" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="state">State</label>
+                        <input className="form-control" type="text" id="state" name="state" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="city">Cuisines</label>
+                        <input className="form-control" type="text" id="cuisines" name="cuisines" required/>
+                    </div>
+                    {/* Create submit button */}
+                    <input className="btn btn-primary" type="submit" value="Add Place" />
+                </form>
             </main>
         </Def>
     )
