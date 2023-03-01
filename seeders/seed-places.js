808593eeec56d4f1e184e5e9b794fd6a9f26abdb
hunnerrose <114459778+hunnerrose@users.noreply.github.com>
2023-02-28 17:09:10 -0800

    added seeders and seed-places file

---
+++ seeders/seed-places.js
@@ -0,0 +1,28 @@
+//Purpose of a seeder file: to provide sample data in a database (for good app demonstration)
+const db = require('./models')
+
+//Use Mongoose's create() method to create a new place. (If we pass it an array, it will create multiple places at once)
+db.Place.create([{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/h-thai-ml-tables.jpg',
+    founded: 1989
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/coffee-cat.jpg',
+    founded: 2020
+}])
+.then( () => {
+    consolee.log('Success!')
+        //call the process.exit() method to let the file know when  the process can be closed so it doesn't just hang open like a web server
+    process.exit()
+})
+.catch(err => {
+    console.log('Failure!', err)
+    process.exit()
+})
